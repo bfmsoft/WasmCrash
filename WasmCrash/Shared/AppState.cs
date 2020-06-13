ec97191b45799e76a6d6be0283985317aac57180
Brett McDonald <brettmc@bfmsoft.com>
2020-06-13 19:47:55 -0400

    Add project files.

---
+++ WasmCrash/Shared/AppState.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WasmCrash.Shared {
+	public class AppState : IAppState {
+        public string EmailAddress
+        {
+            get
+            {
+                return EmailAddress;
+            }
+            set
+            {
+                EmailAddress = value;
+                NotifyStateChanged();
+            }
+        }
+        public string PasswordHash
+        {
+            get
+            {
+                return PasswordHash;
+            }
+            set
+            {
+                PasswordHash = value;
+                NotifyStateChanged();
+            }
+        }
+        public bool IsLoggedIn
+        {
+            get
+            {
+                return !string.IsNullOrWhiteSpace(EmailAddress) && !string.IsNullOrWhiteSpace(PasswordHash);
+            }
+        }
+
+        public event Action? OnChange;
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+    }
+}
---
+++ WasmCrash/Shared/IAppState.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WasmCrash.Shared {
+    public interface IAppState {
+        public string EmailAddress { get; set; }
+        public string PasswordHash { get; set; }
+        public bool IsLoggedIn { get; }
+
+        public event Action OnChange;
+    }
+}
